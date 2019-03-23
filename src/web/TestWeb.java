bebcc9ac06263062510e04d0c0a264c43be8d80e
hluu2024 <darkzero2024@gmail.com>
2019-03-23 15:28:55 -0700

    test web

---
+++ src/web/TestWeb.java
@@ -0,0 +1,32 @@
+package web;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/HelloWorld", "/a", "/b", "/member/*" })
+public class TestWeb extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public TestWeb()
+    {
+        super();
+    }
+
+    protected void doGet( HttpServletRequest request,
+        HttpServletResponse response ) throws ServletException, IOException
+    {
+        PrintWriter out = response.getWriter();
+
+        response.setContentType( "text/html" );
+        out.println( "<html><head><title>Hello</title></head><body>" );
+        out.println( "<h2>Hello World!</h2>" );
+        out.println( "</body></html>" );
+    }
+
+}
