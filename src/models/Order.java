39acf089af5515efbe9efa3cec0fb62c4d499296
ptobgui300 <patriots.stars@gmail.com>
2019-04-13 15:37:36 -0700

    orders changes

--- src/models/Order.java
+++ src/models/Order.java
@@ -1,5 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Order {
 
+	static int count=0;
+	int id;
+	ArrayList<Menu> menu;
+	int totalCost;
+	
+	public Order(ArrayList<Menu> menu) {
+		this.id=count++;
+		this.menu=menu;
+	}
 }
