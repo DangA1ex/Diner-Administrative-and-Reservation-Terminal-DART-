03c2ff40301467546b1ac137486dbb890a4d073c
ptobgui300 <patriots.stars@gmail.com>
2019-04-13 15:25:02 -0700

    constructors for dart started

---
+++ src/models/Ingredients.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Ingredients {
+
+	int quantity;
+	String name;
+	
+	public Ingredients(int quantity, String name) {
+		this.quantity=quantity;
+		this.name= name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
--- src/models/Menu.java
+++ src/models/Menu.java
@@ -1,5 +1,61 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Menu {
 
+	String name;
+	Integer id;
+	ArrayList<String> photo;
+	ArrayList<Ingredients> ingredients;
+	String description;
+	int cost;
+	
+	public Menu(Integer id, String name, String description, int cost, ArrayList<Ingredients> ingredients) {
+		this.id=id;
+		this.name=name;
+		this.cost=cost;
+		this.photo = new ArrayList<String>();
+		this.ingredients=ingredients; 
+	}
+	
+	public ArrayList<Ingredients> getIngredients() {
+		return ingredients;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public ArrayList<String> getPhotos() {
+		return photo;
+	}
+
+	public void setPhoto(ArrayList<String> photo) {
+		this.photo = photo;
+	}
+	
+	public String getPhoto(int i) {
+		return this.photo.get(i);
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
