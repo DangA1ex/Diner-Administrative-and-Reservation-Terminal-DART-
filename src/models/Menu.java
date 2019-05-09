5f0cf4acc49a7494b41a29a970b34a67d7ce5834
ptobgui300 <patriots.stars@gmail.com>
2019-05-08 18:38:15 -0700

    final

--- src/models/Menu.java
+++ src/models/Menu.java
@@ -1,61 +1,93 @@
 package models;
 
-import java.util.ArrayList;
+
 
 public class Menu {
 
-	String name;
 	Integer id;
-	ArrayList<String> photo;
-	ArrayList<Ingredients> ingredients;
-	String description;
+
+	String name;
+
 	Float price;
+
 	Integer quantity;
+
 	
+
 	public Menu (Integer id, String name, Float price, Integer quantity) {
+
 		this.id = id;
+
 		this.name = name;
+
 		this.price = price;
+
 		this.quantity = quantity;
-	}
-	
-	public ArrayList<Ingredients> getIngredients() {
-		return ingredients;
+
 	}
 
-	public Float getPrice() {
-		return price;
-	}
 
-	public String getName() {
-		return name;
-	}
 
 	public Integer getId() {
+
 		return id;
+
 	}
+
+
 
 	public void setId(Integer id) {
+
 		this.id = id;
+
 	}
 
-	public ArrayList<String> getPhotos() {
-		return photo;
+
+
+	public String getName() {
+
+		return name;
+
 	}
 
-	public void setPhoto(ArrayList<String> photo) {
-		this.photo = photo;
-	}
-	
-	public String getPhoto(int i) {
-		return this.photo.get(i);
+
+
+	public void setName(String name) {
+
+		this.name = name;
+
 	}
 
-	public String getDescription() {
-		return description;
+
+
+	public Float getPrice() {
+
+		return price;
+
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+
+
+	public void setPrice(Float price) {
+
+		this.price = price;
+
 	}
+
+
+
+	public Integer getQuantity() {
+
+		return quantity;
+
+	}
+
+
+
+	public void setQuantity(Integer quantity) {
+
+		this.quantity = quantity;
+
+	}
+
 }
